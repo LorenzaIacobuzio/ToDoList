796598508a31facece19ce2f696fc692f47c6be0
Lorenza Iacobuzio <lorenza.iacobuzio@medable.com>
2024-01-19 11:27:25 +0100

    Create database connection and update readme

--- README.md
+++ README.md
@@ -25,11 +25,28 @@ A simple backend system for a to-do list app, with a RESTful API and a database.
 * Docker 24 or later
 * Git
 * Intellij 2023
-* Postgres for Ubuntu
+* Postgres 12 for Ubuntu (to install, follow https://www.postgresql.org/download/linux/ubuntu/)
 
 ## Build and run
 
+To build and run the app with gradle, do `./gradlew run assemble` in the root directory of the project.
+
 ## Test
+
+ To run all project tests, do `./gradlew clean test`.
+
+## Database
+
+Switch to system admin from command line with `sudo -i -u postgres`.
+This user can create new databases, users, etc.
+
+To create a new local database instance, do `createdb myDbName`.
+
+You can access the new database via the interactive terminal program, by doing `psql myDbName`.
+
+You can also connect Intellij to your database (ToDoListDB in this case), by using the right-hand side menu in the IDE (credentials are in the application.conf file).
+
+The application is now connected to the new database.
 
 ## Spec requirements
 
--- src/main/kotlin/com/todolist/Application.kt
+++ src/main/kotlin/com/todolist/Application.kt
@@ -1,5 +1,6 @@
 package com.todolist
 
+import com.typesafe.config.ConfigFactory
 import com.todolist.plugins.*
 import getStatusRoute
 import io.ktor.server.application.*
@@ -7,6 +8,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
@@ -14,9 +18,25 @@ fun main() {
 }
 
 fun Application.module() {
+    DatabaseFactory.init()
     routing {
         route("/v1") {
             getStatusRoute()
+        }
+    }
+}
+
+object DatabaseFactory {
+    fun init() {
+        val database =  Database.connect(
+            url = "jdbc:postgresql://localhost:5432/ToDoListDB",
+            driver = "org.postgresql.Driver",
+            user = "postgres",
+            password = "postgres"
+        )
+
+        transaction (database) {
+            //SchemaUtils.create()
         }
     }
 }
